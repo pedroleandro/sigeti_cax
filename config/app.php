8736ca9183281d80fa746433787a040339403a04
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2026-03-10 22:52:15 -0300

    feat: implementa conexão com o banco de dados

--- config/app.php
+++ config/app.php
@@ -1,1 +1,16 @@
 <?php
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
+define("APP_URL", $_ENV['APP_URL'] ?? "http://localhost/sigeti_cax");
+
+define("DB_CONNECTION", $_ENV['DB_CONNECTION'] ?? "yourdbsql");
+define("DB_HOST", $_ENV['DB_HOST'] ?? "yourhost.com");
+define("DB_PORT", $_ENV['DB_PORT'] ?? "9999");
+define("DB_DATABASE", $_ENV['DB_DATABASE'] ?? "dbname");
+define("DB_USERNAME", $_ENV['DB_USERNAME'] ?? "user");
+define("DB_PASSWORD", $_ENV['DB_PASSWORD'] ?? "password");
+define("DB_CHARSET", $_ENV['DB_CHARSET'] ?? "utf8mb4");
--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+try {
+
+    $connection = \App\Core\Connection::getInstance();
+
+    var_dump($connection);
+
+} catch (\RuntimeException $exception) {
+    echo $exception->getMessage();
+}
