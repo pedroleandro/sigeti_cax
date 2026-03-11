10b6d51e2cf8dacd456b7537881e4d3dc2b05b8a
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2026-03-11 09:33:04 -0300

    feat: instala template do site.home sigeti_cax

--- app/Helpers/url.php
+++ app/Helpers/url.php
@@ -11,6 +11,17 @@ function url(string $path = null): string
     return $base;
 }
 
+function assets(string $path = null): string
+{
+    $base = APP_URL . "/public/assets";
+
+    if ($path) {
+        return $base . '/' . ltrim($path, '/');
+    }
+
+    return $base;
+}
+
 function redirect(string $path): void
 {
     header("Location: " . url($path));
