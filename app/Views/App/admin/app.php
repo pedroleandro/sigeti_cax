82f19153e3e067e22b6f13da903fcfea6a1acc01
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2026-03-12 09:43:07 -0300

    feat: confecciona página de dashboard do tecnico

--- app/Views/App/admin/app.php
+++ app/Views/App/admin/app.php
@@ -1,1 +1,253 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?= $title ?></title>
+
+    <!-- Custom fonts for this template-->
+    <link href="<?= assets_sb_admin('/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?= assets_sb_admin('/css/sb-admin-2.min.css') ?>" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center"
+           href="<?= url('/professor/dashboard') ?>">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">SIGETI <sup>CAX</sup></div>
+        </a>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="<?= url('/professor/dashboard') ?>">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Área do Técnico</span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Sistema
+        </div>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#schools"
+               aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-school fa-cog"></i>
+                <span>Escolas</span>
+            </a>
+            <div id="schools" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="">Novo</a>
+                    <a class="collapse-item" href="">Todos</a>
+                </div>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#users"
+               aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-user fa-cog"></i>
+                <span>Usuários</span>
+            </a>
+            <div id="users" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="">Novo</a>
+                    <a class="collapse-item" href="">Todos</a>
+                </div>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#categories"
+               aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-cog"></i>
+                <span>Categorias</span>
+            </a>
+            <div id="categories" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="">Novo</a>
+                    <a class="collapse-item" href="">Todos</a>
+                </div>
+            </div>
+        </li>
+
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#tickets"
+               aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-envelope fa-cog"></i>
+                <span>Chamados</span>
+            </a>
+            <div id="tickets" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="">Novo</a>
+                    <a class="collapse-item" href="">Todos</a>
+                    <a class="collapse-item" href="">Abertos</a>
+                    <a class="collapse-item" href="">Em andamento</a>
+                    <a class="collapse-item" href="">Finalizados</a>
+                    <a class="collapse-item" href="">Arquivados</a>
+                </div>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="modal" data-target="#logoutModal">
+                <i class="fas fa-sign-out-alt fa-fw fa-cog"></i>
+                <span>Sair</span>
+            </a>
+        </li>
+
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                <!-- Sidebar Toggle (Topbar) -->
+                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                    <i class="fa fa-bars"></i>
+                </button>
+
+                <!-- Topbar Navbar -->
+                <ul class="navbar-nav ml-auto">
+
+                    <div class="topbar-divider d-none d-sm-block"></div>
+
+                    <!-- Nav Item - User Information -->
+                    <li class="nav-item dropdown no-arrow">
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">João Pedro</span>
+                            <img class="img-profile rounded-circle"
+                                 src="<?= assets_sb_admin('/img/undraw_profile.svg') ?>">
+                        </a>
+                        <!-- Dropdown - User Information -->
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                             aria-labelledby="userDropdown">
+                            <a class="dropdown-item" href="">
+                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Perfil
+                            </a>
+                            <a class="dropdown-item" href="">
+                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Configurações
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="" data-toggle="modal" data-target="#logoutModal">
+                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Sair
+                            </a>
+
+                        </div>
+                    </li>
+
+                </ul>
+
+            </nav>
+            <!-- End of Topbar -->
+
+            <?= $this->section('content') ?>
+
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span><?= date('Y') . " - " . APP_NAME ?></span>
+                </div>
+            </div>
+        </footer>
+        <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Pronto para ir?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Selecione "Sair" abaixo se você estiver pronto para deixar a sessão atual.</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+                <form action="<?= url('/sair') ?>" method="post">
+                    <?= csrf_input() ?>
+                    <button type="submit" class="btn btn-primary">Sair</button>
+                </form>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="<?= assets_sb_admin('/vendor/jquery/jquery.min.js') ?>"></script>
+<script src="<?= assets_sb_admin('/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?= assets_sb_admin('/vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= assets_sb_admin('/js/sb-admin-2.min.js') ?>"></script>
+
+<!-- Page level plugins -->
+<script src="<?= assets_sb_admin('/vendor/chart.js/Chart.min.js') ?>"></script>
+
+<!-- Page level custom scripts -->
+<script src="<?= assets_sb_admin('/js/demo/chart-area-demo.js') ?>"></script>
+<script src="<?= assets_sb_admin('/js/demo/chart-pie-demo.js') ?>"></script>
+
+</body>
+
+</html>
