06a190ed7ae651042d54a85eacc206096c718e8c
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2026-03-26 18:01:24 -0300

    refactor: refatora modelo de user

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,12 +17,36 @@ class User extends AbstractModel
         "last_login_at",
         "status"
     ];
+
     protected array $required = [
-        "name" => "O nome é obrigatório.",
-        "email" => "O email é obrigatório.",
-        "password" => "A senha é obrigatória."
+        "name" => "O campo NOME é obrigatório.",
+        "email" => "O campo EMAIL é obrigatório.",
+        "password" => "O campo SENHA é obrigatório."
     ];
+
     protected bool $timestamps = true;
+
+    public const TEACHER = "professor";
+    public const TECHNICIAN = "tecnico";
+
+    private const ROLES = [
+        self::TEACHER,
+        self::TECHNICIAN
+    ];
+
+    public const REGISTERED = "registrado";
+    public const ACTIVE = "ativo";
+    public const INACTIVE = "inativo";
+    private const STATUS = [
+        self::REGISTERED,
+        self::ACTIVE,
+        self::INACTIVE
+    ];
+
+    public function getId(): ?int
+    {
+        return $this->attributes["id"];
+    }
 
     public function setName(string $name): void
     {
@@ -33,6 +57,11 @@ class User extends AbstractModel
         $this->attributes["name"] = $name;
     }
 
+    public function getName(): ?string
+    {
+        return $this->attributes["name"] ?? null;
+    }
+
     public function setEmail(string $email): void
     {
         $email = filter_var(trim($email), FILTER_VALIDATE_EMAIL);
@@ -40,6 +69,11 @@ class User extends AbstractModel
             throw new \InvalidArgumentException("E-mail inválido");
         }
         $this->attributes["email"] = $email;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->attributes["email"] ?? null;
     }
 
     public function setPassword(?string $password): void
@@ -64,6 +98,11 @@ class User extends AbstractModel
         $this->attributes["document"] = $document;
     }
 
+    public function getDocument(): ?string
+    {
+        return $this->attributes["document"] ?? null;
+    }
+
     public function setRole(string $role): void
     {
         $roles = ["professor", "tecnico"];
@@ -71,6 +110,11 @@ class User extends AbstractModel
             throw new \InvalidArgumentException("Perfil inválido");
         }
         $this->attributes["role"] = $role;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->attributes["role"] ?? null;
     }
 
     public function setStatus(?string $status): void
@@ -83,41 +127,16 @@ class User extends AbstractModel
         $this->attributes["status"] = $status;
     }
 
+    public function getStatus(): ?string
+    {
+        return $this->attributes["status"] ?? null;
+    }
+
     public function setLastLoginAt(): void
     {
         $timezone = new \DateTimeZone($_ENV['APP_TIMEZONE'] ?? 'America/Sao_Paulo');
         $now = new \DateTimeImmutable('now', $timezone);
         $this->attributes["last_login_at"] = $now->format('Y-m-d H:i:s');
-    }
-
-    public function getId(): ?int
-    {
-        return $this->attributes["id"] ?? null;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->attributes["name"] ?? null;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->attributes["email"] ?? null;
-    }
-
-    public function getDocument(): ?string
-    {
-        return $this->attributes["document"] ?? null;
-    }
-
-    public function getRole(): ?string
-    {
-        return $this->attributes["role"] ?? null;
-    }
-
-    public function getStatus(): ?string
-    {
-        return $this->attributes["status"] ?? null;
     }
 
     public function getLastLogin(): mixed
